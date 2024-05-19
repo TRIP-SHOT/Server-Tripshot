ec4897f1f8db1db1d5d0525f91a4d9736a62d90f
hoiae <donald819@naver.com>
2024-05-20 00:35:08 +0900

    test

--- src/main/java/com/tripshot/TripshotApplication.java
+++ src/main/java/com/tripshot/TripshotApplication.java
@@ -2,11 +2,9 @@ package com.tripshot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 public class TripshotApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TripshotApplication.class, args);
 	}
