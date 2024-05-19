0993a47fb1b583cc690f4783ca92db18a7ce7446
hoiae <donald819@naver.com>
2024-05-20 02:34:12 +0900

    chore:의존성추가

--- src/main/java/com/tripshot/temp/controller/MainController.java
+++ src/main/java/com/tripshot/temp/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.temp;
+package com.tripshot.temp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @ResponseBody
-public class AdminController {
-    @GetMapping("/admin")
-    public String adminP(){
-        return "admin";
+public class MainController {
+    @GetMapping("/")
+    public String mainP(){
+        return "Main Controller";
     }
 }
