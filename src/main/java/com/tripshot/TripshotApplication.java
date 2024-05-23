3850358801f83efccc73205b93814309d7141b35
hoiae <donald819@naver.com>
2024-05-23 16:02:39 +0900

    feat: swagger auth swagger에서 헤더에 토큰을 쉽게 넣어서 확인할 수 있는 설정추가

--- src/main/java/com/tripshot/TripshotApplication.java
+++ src/main/java/com/tripshot/TripshotApplication.java
@@ -2,11 +2,33 @@ package com.tripshot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @SpringBootApplication
 public class TripshotApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TripshotApplication.class, args);
 	}
+	
+	
+	 @Bean
+	  public OpenAPI api() {
+	    SecurityScheme apiKey = new SecurityScheme()
+	      .type(SecurityScheme.Type.APIKEY)
+	      .in(SecurityScheme.In.HEADER)
+	      .name("Authorization");
+
+	    SecurityRequirement securityRequirement = new SecurityRequirement()
+	      .addList("Bearer Token");
+
+	    return new OpenAPI()
+	      .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
+	      .addSecurityItem(securityRequirement);
+	  }
 
 }
